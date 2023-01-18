ea83bc65898262fba8ed725d1424e1789eeceebd
wocjs <wocjs602@gmail.com>
2023-01-18 23:20:03 +0900

    2003_H

--- Code/p2003/2003_H.py
+++ Code/p2003/2003_H.py
@@ -1,17 +1,14 @@
 m, n = map(int, input().split())
 arr = list(map(int, input().split()))
-Sum = cnt = ptS = ptE = 0                 # count, pointer_Start, pointer_End
+Sum = cnt = ptS = ptE = 0
 
-while ptS != m and ptE != m+1:                   # ptS 배열 끝까지 갔을 때 종료
+while ptS != m and ptE != m+1:
     Sum = sum(arr[ptS:ptE])
-    if Sum == n:      # ptS과 ptE 사이 합이 같을 때 cnt ++, ptS ++
+    if Sum == n:
         cnt += 1
         ptS += 1
-        continue
-    elif Sum < n:     # 사이 합이 n값보다 작을 때 끝점을 늘려 부분합 증가
-        ptE += 1      # ptE가 끝까지 갔을 때 sum보다 작으면 ptE 추가되서 무한루프,,
-        continue
+    elif Sum < n:
+        ptE += 1
     else:
         ptS += 1
-        continue
 print(cnt)
