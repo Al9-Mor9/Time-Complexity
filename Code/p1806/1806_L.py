218e6e5db28b4cf297e268ba01a34ba7d3038568
unknown <suhyeng119@gmail.com>
2023-07-07 13:10:49 +0900

    L

--- Code/p1806/1806_L.py
+++ Code/p1806/1806_L.py
@@ -12,14 +12,18 @@ ans = int(1e5 + 1)
 p1, p2 = 0, 1
 tmp = arr[0]
 
-while p2 < n:
+
+while p1 < n and p2 <= n:
+    print(p1, p2, tmp)
     if tmp >= s:
         ans = min(ans, p2 - p1)
         tmp -= arr[p1]
         p1 += 1
     else:
+        if p2 == n:
+            break
+        tmp += arr[p2]
         p2 += 1
-        tmp += arr[p2 - 1]
 
 print(0) if ans == int(1e5 + 1) else print(ans)
 
